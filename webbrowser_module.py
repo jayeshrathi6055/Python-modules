cf440158b9f4e0425582efe66ed8cb8ec2b30179
jayeshrathi6055 <65055605+jayeshrathi6055@users.noreply.github.com>
2020-10-25 09:11:05 +0530

    Add files via upload

---
+++ webbrowser_module.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+# # Open any url------------------------------------------------------------------------>
+
+# webbrowser.open('https://docs.python.org/3/library/webbrowser.html')
+# webbrowser.open_new_tab('https://docs.python.org/3/library/webbrowser.html')------>open in new tab
+# webbrowser.open_new('https://docs.python.org/3/library/webbrowser.html') ------>open in new tab
+
+# # Open url with specific browser------------------------------------------------------->
+
+# b = webbrowser.get('windows-default')
+# b.open('https://docs.python.org/3/library/webbrowser.html')
