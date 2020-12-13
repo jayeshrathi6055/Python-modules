982c745b6f14b0998cc930bd9d923c7e7a2b30fd
jayeshrathi6055 <65055605+jayeshrathi6055@users.noreply.github.com>
2020-12-13 13:52:12 +0530

    Add files via upload

---
+++ os_module.py
@@ -0,0 +1,28 @@
+import os
+
+# # Check your working directory
+# print(os.getcwd())
+
+# # Want to rename the file
+# os.rename("hi.txt","hello.txt")
+
+# # Want to change directory
+# os.chdir(r"C:\Users\Aman Rathi\Desktop\Projects")
+# print(os.getcwd())
+
+# # Want to make new folder
+# os.mkdir("test")
+##   or
+# os.makedirs("test")
+
+# # Want to delete any file
+# os.remove("hello.txt")
+
+# # want to delete any folder
+# os.rmdir("test")
+
+# # It will open file
+# os.startfile("hello.txt")
+
+# # It will show all files and folders present in a working directory
+# print(os.listdir())
